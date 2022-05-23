ab2c917f2cec44618e310d29d4bddfb23748e548
riccardo-negri <67798955+riccardo-negri@users.noreply.github.com>
2022-05-23 22:40:25 +0200

    Dumb connection to DB working, finally

---
+++ code/pure-html/src/main/java/it/polimi/tiw/controllers/CheckLogin.java
@@ -0,0 +1,34 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CheckLogin")
+public class CheckLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		final String DB_URL = "jdbc:mysql://localhost:3306/dbtest?serverTimezone=UTC";
+		final String USER = "admin";
+		final String PASS = "password";
+		String result = "Connection worked, we made it!!!!!";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			DriverManager.getConnection(DB_URL, USER, PASS);
+		} catch (Exception e) {
+			result = "Connection failed";
+			e.printStackTrace();
+		}
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		out.println(result);
+		out.close();
+	}
+}
